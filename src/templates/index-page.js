f7b87f0ed9bce71e2f28150bf2c843152b988b33
Tina <longevity.tina@gmail.com>
2021-09-12 19:07:14 -0700

    edit

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,6 +5,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+// import Img from "gatsby-image/withIEPolyfill"
 
 export const IndexPageTemplate = ({
   image,
