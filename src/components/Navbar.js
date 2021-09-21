57ef98439fd44512b8d53329b85db725d51f157b
Tina <longevity.tina@gmail.com>
2021-09-20 20:20:02 -0700

    Styles nav bar and projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,8 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              Home
+              {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
             </Link>
             {/* Hamburger menu */}
             <div
